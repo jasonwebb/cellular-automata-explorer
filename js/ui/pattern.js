93e7733f0209ad6e9422eff763420795203d1cbb
Jason Webb <zen.webb@gmail.com>
2021-05-23 16:38:58 -0500

    Populating Starting Pattern dropdown dynamically from variables

--- js/ui/pattern.js
+++ js/ui/pattern.js
@@ -1,12 +1,19 @@
-import { createGroup, createDropdown, createButton, createSlider, createCheckbox } from './components';
+import { createGroup, createDropdown } from './components';
+import variables from '../variables';
 
 export function createPatternGroup() {
   let group = createGroup('Starting pattern');
 
+  // Create an array of strings from the pattern keys
+  let patternsSimpleList = [];
+  Object.keys(variables.patterns).forEach((pattern) => {
+    patternsSimpleList.push(pattern.charAt(0).toUpperCase() + pattern.slice(1, pattern.length));
+  });
+
   // Dropdown for patterns
   group.appendChild(
-    createDropdown('Starting pattern', ['test'], () => {
-      console.log('starting pattern changed');
+    createDropdown('Starting pattern', patternsSimpleList, (e) => {
+      console.log('starting pattern changed to: ' + e.target.value);
     })
   );
 
