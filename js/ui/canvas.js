9d56da1341f36ee8676211fc4b491bd438241209
Jason Webb <zen.webb@gmail.com>
2021-05-23 14:36:47 -0500

    Resizing the canvas whenever width, height, or maximized controls changed

--- js/ui/canvas.js
+++ js/ui/canvas.js
@@ -1,27 +1,31 @@
-import { createGroup, createDropdown, createButton, createSlider, createCheckbox } from './components';
+import { createGroup, createSlider, createCheckbox } from './components';
 import variables from '../variables'
+import { resetTextureSizes } from '../entry';
 
 export function createCanvasGroup() {
   let group = createGroup('Canvas');
 
   // Width slider
   group.appendChild(
-    createSlider('Width', 1, variables.canvas.width.max, 1, variables.canvas.width.value, () => {
-      console.log('canvas width changed');
+    createSlider('Width', 1, variables.canvas.width.max, 1, variables.canvas.width.value, (e) => {
+      variables.canvas.width.value = e.target.value;
+      resetTextureSizes();
     })
   );
 
   // Height slider
   group.appendChild(
-    createSlider('Height', 1, variables.canvas.height.max, 1, variables.canvas.height.value, () => {
-      console.log('canvas height changed');
+    createSlider('Height', 1, variables.canvas.height.max, 1, variables.canvas.height.value, (e) => {
+      variables.canvas.height.value = e.target.value;
+      resetTextureSizes();
     })
   );
 
   // Maximized checkbox
   group.appendChild(
-    createCheckbox('Maximized', variables.canvas.maximized, () => {
-      console.log('maximized changed');
+    createCheckbox('Maximized', variables.canvas.maximized, (e) => {
+      variables.canvas.maximized = e.target.value;
+      resetTextureSizes();
     })
   );
 
