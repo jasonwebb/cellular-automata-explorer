57762275821414ccefb7fa74c4a79bb2dbf3230c
Jason Webb <zen.webb@gmail.com>
2021-05-23 16:37:10 -0500

    Abstracting colors to flexible multi-dimensional array

--- js/colors.js
+++ js/colors.js
@@ -2,21 +2,19 @@ import * as THREE from 'three';
 
 import { displayUniforms } from "./uniforms";
 
+export let colors = [
+  [0, .3, .2, 1],  // state 0 color
+  [1, .8, .4, 1]   // state 1 color
+]
+
 export function setColors() {
-  const numColors = 2;
-  let data = new Float32Array(numColors * 4);  // RGBA = 4 channels
+  let data = new Float32Array(colors.length * 4);  // RGBA = 4 channels
 
-  // State 0 color
-  data[0] = 0;
-  data[1] = .3;
-  data[2] = .2;
-  data[3] = 1;
+  colors.forEach((color, offset) => {
+    for(let i=0; i<=color.length; i++) {
+      data[i + offset * color.length] = color[i];
+    }
+  });
 
-  // Sate 1 color
-  data[4] = 1;
-  data[5] = .8;
-  data[6] = .4;
-  data[7] = 1;
-
-  displayUniforms.colors.value = new THREE.DataTexture(data, numColors, 1, THREE.RGBAFormat, THREE.FloatType);
+  displayUniforms.colors.value = new THREE.DataTexture(data, colors.length, 1, THREE.RGBAFormat, THREE.FloatType);
 }
