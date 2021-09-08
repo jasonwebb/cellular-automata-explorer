2926b63e3256e4b8e1c702b0696739d74c74c352
Jason Webb <zen.webb@gmail.com>
2021-09-08 18:57:38 -0500

    Rebuilding controls on global rebuild event so that play/pause button accurately updates

--- js/ui/controls.js
+++ js/ui/controls.js
@@ -6,46 +6,53 @@ import globals from '../globals';
 export function createControlsGroup() {
   let group = createGroup('Controls');
 
-  // Speed slider
-  group.appendChild(
-    createSlider('Speed', 0.1, 2.0, .1, 1.0, () => {
-      console.log('speed changed');
-    })
-  );
+  window.addEventListener('rebuildUI', () => {
+    // Get rid of any previously-added components
+    group.querySelectorAll('.component, .row').forEach((component) => {
+      component.remove();
+    });
 
-  // Row for laying out buttons horizontally
-  let buttonRow = createRow();
+    // Speed slider
+    group.appendChild(
+      createSlider('Speed', 0.001, 2.0, .1, globals.speedMultiplier, (e) => {
+        globals.speedMultiplier = e.target.value;
+      })
+    );
 
-  // Pause button
-  let pauseButton = createToggleButton(`
-    <span class="fas fa-pause" aria-hidden="true"></span>
-    <span class="fas fa-play" aria-hidden="true"></span>
-    <span class="text pause">Pause</span>
-    <span class="text play">Play</span>
-  `, globals.isPaused, () => {
-    globals.isPaused = !globals.isPaused;
+    // Row for laying out buttons horizontally
+    let buttonRow = createRow();
+
+    // Pause button
+    let pauseButton = createToggleButton(`
+      <span class="fas fa-pause" aria-hidden="true"></span>
+      <span class="fas fa-play" aria-hidden="true"></span>
+      <span class="text pause">Pause</span>
+      <span class="text play">Play</span>
+    `, globals.isPaused, () => {
+      globals.isPaused = !globals.isPaused;
+    });
+
+      pauseButton.querySelector('button').setAttribute('aria-label', 'Pause');
+
+    // Restart button
+    let restartButton = createButton(`
+      <span class="fas fa-sync-alt" aria-hidden="true"></span>
+      <span class="text">Restart</span>
+    `, false, () => {
+      setupRenderTargets();
+      drawPattern();
+    });
+
+    pauseButton.classList.add('is-control-button');
+    restartButton.classList.add('is-control-button');
+
+    pauseButton.querySelector('button').setAttribute('id', 'pause-button');
+    restartButton.querySelector('button').setAttribute('id', 'restart-button');
+
+    buttonRow.appendChild(pauseButton);
+    buttonRow.appendChild(restartButton);
+    group.appendChild(buttonRow);
   });
-
-    pauseButton.querySelector('button').setAttribute('aria-label', 'Pause');
-
-  // Restart button
-  let restartButton = createButton(`
-    <span class="fas fa-sync-alt" aria-hidden="true"></span>
-    <span class="text">Restart</span>
-  `, false, () => {
-    setupRenderTargets();
-    drawPattern();
-  });
-
-  pauseButton.classList.add('is-control-button');
-  restartButton.classList.add('is-control-button');
-
-  pauseButton.querySelector('button').setAttribute('id', 'pause-button');
-  restartButton.querySelector('button').setAttribute('id', 'restart-button');
-
-  buttonRow.appendChild(pauseButton);
-  buttonRow.appendChild(restartButton);
-  group.appendChild(buttonRow);
 
   return group;
 }
