7b5c51e70372fd58fa55a0ba3bfb3661f64335cd
Jason Webb <zen.webb@gmail.com>
2021-09-08 17:23:39 -0500

    Eliminating the rule string text field

--- js/ui/rules.js
+++ js/ui/rules.js
@@ -1,7 +1,6 @@
-import { createGroup, createDropdown, createTextInput, createButton, createSeperator } from './components';
+import { createGroup, createDropdown } from './components';
 import { setRule } from '../rules';
 import presets from '../presets';
-import variables from '../variables';
 
 export function createRulesGroup() {
   let group = createGroup('Rules');
@@ -24,34 +23,6 @@ export function createRulesGroup() {
           setRule(presets[family][e.target.value]);
         }
       }
-    })
-  );
-
-    group.appendChild(createSeperator());
-
-  // Text input for the rule string
-  group.appendChild(
-    createTextInput('Rule', '', (e) => {
-      console.log('rule text changed');
-    })
-  );
-
-    // Set the rule when the user presses 'Enter' while on the text input
-    group.querySelector('.text-input input').addEventListener('keydown', (e) => {
-      if(e.key === 'Enter') {
-        setRule(group.querySelector('.text-input input').value);
-      }
-    })
-
-    // Show the current rule string in this text input anytime it changes
-    window.addEventListener('ruleUpdated', () => {
-      group.querySelector('.text-input input').value = variables.activeRule.ruleString;
-    });
-
-  // Button to manually set a custom rule typed into the text input
-  group.appendChild(
-    createButton('Set rule', true, () => {
-      setRule(group.querySelector('.text-input input').value);
     })
   );
 
